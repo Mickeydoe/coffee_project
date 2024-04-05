42adfff061b2e0c98bac655c8b6a98b2d7cb14e5
Mickeydoe <michaeldanquah615@gmail.com>
2024-04-05 01:12:42 +0000

    Userdata additions

--- intance.tf
+++ intance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web_instance" {
   ami = "ami-080e1f13689e07408"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
+  user_data     = file("${path.module}/webserver_userdata.sh") # Include user data
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
 
